822b9fc61da1a4864da05c1e42470a2ec06ad69a
hemanth <sp2013hem@gmail.com>
2023-06-15 07:28:02 +0200

    add webpack

---
+++ js/app.js
@@ -0,0 +1,177 @@
+import "../css/fonts.css";
+import "../css/main.css";
+
+const menuBtn = document.querySelector(".menu-button");
+const overlay = document.querySelector(".w-nav-overlay");
+const target = document.getElementById("preloader");
+
+const loadParticles = (url) => {
+  const script = document.createElement("script");
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  document.body.appendChild(script);
+
+  script.onload = renderParticles;
+};
+const renderParticles = () => {
+  particlesJS("particles", {
+    particles: {
+      number: {
+        value: 100,
+        density: {
+          enable: false,
+          value_area: 4498.141557303954,
+        },
+      },
+      color: {
+        value: "#03cdac",
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#03cdac",
+        },
+        polygon: {
+          nb_sides: 6,
+        },
+      },
+      opacity: {
+        value: 0.7,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false,
+        },
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false,
+        },
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#F8F8FF",
+        opacity: 0.05,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 4,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse",
+        },
+        onclick: {
+          enable: true,
+          mode: "push",
+        },
+        resize: true,
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1,
+          },
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+        push: {
+          particles_nb: 4,
+        },
+        remove: {
+          particles_nb: 2,
+        },
+      },
+    },
+    retina_detect: true,
+  });
+};
+if (!!target) {
+  const hidePreLoader = () => {
+    setTimeout(() => target.remove(), 1500);
+    try {
+      target.style.opacity = "0";
+      target.addEventListener("transitionend", () => target.remove());
+    } catch (error) {}
+  };
+  setTimeout(hidePreLoader, 10);
+}
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    // e.preventDefault();
+
+    const element = document.querySelector(this.getAttribute("href"));
+    const headerOffset = 100;
+    const elementPosition = element.getBoundingClientRect().top;
+    const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+    window.scrollTo({ top: offsetPosition, behavior: "smooth" });
+  });
+});
+
+if (document.querySelector("form")) {
+  document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    }).then(() =>
+      alert("Thank you for your submission! I'll get back to you soon.")
+    );
+  });
+}
+
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("w--open");
+  overlay.style.display = overlay.style.display === "block" ? "none" : "block";
+});
+overlay.addEventListener("click", function () {
+  menuBtn.classList.remove("w--open");
+  overlay.style.display = "none";
+});
+if (
+  !window.matchMedia("(max-width: 767px)").matches &&
+  !!document.getElementById("particles")
+)
+  loadParticles("https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js");
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,34 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
 module.exports = {
-  entry: "./src/index.js",
+  mode: "production",
+  plugins: [new MiniCssExtractPlugin()],
+  entry: {
+    app: path.resolve(__dirname, "js/app.js"),
+    geofree: path.resolve(__dirname, "js/geofree.js"),
+  },
   output: {
-    filename: "app.js",
+    filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /[\\/]node_modules[\\/]/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
   },
 };
